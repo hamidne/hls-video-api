8d62f6727d5de75af5ebdb21081649b268fda9c2
HamidNE <hamidne@mail.ru>
2020-01-16 11:19:09 +0330

    add delete video resource

--- app/Http/Controllers/Video/VideoDeleteController.php
+++ app/Http/Controllers/Video/VideoDeleteController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Video;
 
 use App\Http\Controllers\Controller;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoDeleteController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+	/**
+	 * Handle the incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param Video $video
+	 * @return \Illuminate\Http\Response
+	 * @throws \Exception
+	 */
+    public function __invoke(Request $request, Video $video)
     {
-        //
+        $video->delete();
+        return response()->setStatusCode(204);
     }
 }
