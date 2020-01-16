268e7c6c96fe4f3bf1dad5a759b14c3f31c59c9e
HamidNE <hamidne@mail.ru>
2020-01-16 10:44:50 +0330

    fixed videos index route

--- app/Http/Controllers/Video/VideoIndexController.php
+++ app/Http/Controllers/Video/VideoIndexController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers\Video;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Video\VideoIndexResource;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoIndexController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
+	/**
+	 * Handle the incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+	 */
     public function __invoke(Request $request)
     {
-        return view('videos.index');
+        $videos = Video::query()->paginate();
+        return VideoIndexResource::collection($videos);
     }
 }
